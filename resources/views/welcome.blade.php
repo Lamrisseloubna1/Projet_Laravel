e57098b34f125772de4086a53fc36e38378a8fee
Lamrisseloubna1 <lamrisse.loubna245@gmail.com>
2023-12-15 00:51:31 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Votre titre ici</title>
+</head>
+<body>
+    <?php echo file_get_contents(public_path('index.html')); ?>
+</body>
+</html>
