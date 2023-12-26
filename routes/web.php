77dc8e2196f7a9b05269d6b29afaecd234e4d7bf
Lamrisseloubna1 <lamrisse.loubna245@gmail.com>
2023-12-27 00:05:07 +0100

    sécurité des pages

--- routes/web.php
+++ routes/web.php
@@ -28,12 +28,15 @@ Auth::routes();
 
 Route::get('forgot-password', [App\Http\Controllers\ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
 Route::post('forgot-password', [App\Http\Controllers\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
-Route::resource('tasks', TaskController::class);
-Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
-Route::get('/tasks/show', [TaskController::class, 'show'])->name('tasks.show');
-Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
 
-Route::get('/chat', [App\Http\Controllers\ChatController::class,'index'])->name('chat.index');
-Route::post('/chat/send', [App\Http\Controllers\ChatController::class,'sendMessage'])->name('chat.sendMessage');
-Route::get('/chat/messages',  [App\Http\Controllers\ChatController::class,'getMesssage'])->name('chat.getMessage');
-
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::post('/profile', [App\Http\Controllers\Auth\ProfileController::class,'profile'])->name('profile');
+    Route::post('/logout', [App\Http\Controllers\Auth\LoginController::class,'logout'])->name('logout');
+    Route::resource('tasks', TaskController::class);
+    Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+    Route::get('/tasks/show', [TaskController::class, 'show'])->name('tasks.show');
+    Route::get('/chat', [App\Http\Controllers\ChatController::class,'index'])->name('chat');
+    Route::post('/chat/send', [App\Http\Controllers\ChatController::class,'sendMessage'])->name('chat.sendMessage');
+    Route::get('/chat/messages',  [App\Http\Controllers\ChatController::class,'getMesssage'])->name('chat.getMessage');
+});
